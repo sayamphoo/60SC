e8637d6fd12cb1d35d153322af2beb2179b791e7
Sayamphoo <Sayamphoo781@gmail,com>
2021-01-03 13:20:40 +0700

    dataupdata 03/01/2021

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
-var x = document.getElementById("myAudio");
-var playAudio = document.getElementById("AudioPlay");
+var firebaseConfig = {
+  apiKey: "AIzaSyAwRE14s_9bTId5aEa5g_Vld3BJpc4NRi0",
+  authDomain: "august-now-300601.firebaseapp.com",
+  projectId: "august-now-300601",
+  storageBucket: "august-now-300601.appspot.com",
+  messagingSenderId: "414450817854",
+  appId: "1:414450817854:web:8cf42b2dc2084b806c2a6f",
+  measurementId: "G-T6KKWPWXKV"
+};
 
-if (playAudio) {
-  playAudio.addEventListener("click", myPlay, false);
+firebase.initializeApp(firebaseConfig);
+var database = firebase.firestore();
+let date = new Date();
+
+function boxCollection(ip) {
+  let boxCollection = {
+    ip_address: ip,
+    device: getDevice(),
+    date: getData(),
+    time: getTime()
+  };
+
+  return boxCollection;
 }
 
-let point = 0;
-function myPlay() {
-  if (point === 0) {
-    playAudio.src = "img/pause-button.png";
-    x.play();
-    point = 1;
-  } else {
-    playAudio.src = "img/play-button.png";
-    x.pause();
-    point = 0;
-  }
+function getTime() {
+  let h = date.getHours();
+  let m = date.getMinutes();
+  return `${h}:${m}`;
+}
+
+function getData() {
+  let d = date.getDate();
+  let m = date.getMonth() + 1;
+  let y = date.getFullYear();
+  return `${y}${m}${d}`;
+}
+
+function getDevice() {
+  var device =
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/Windows/i);
+
+  return device[0];
+}
+getIPAddress();
+function getIPAddress() {
+  fetch("https://api.ipify.org/?format=json")
+    .then(function(response) {
+      return response.json(); // แปลงข้อมูลที่ได้เป็น json
+    })
+    .then(function(data) {
+      database
+        .collection(getData())
+        .doc(getTime())
+        .set(boxCollection(data.ip));
+   
+    });
 }
